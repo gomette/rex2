cbd9d8761d823907939f30de4e08b57c5b654ef5
gomette <luftlaura@gmail.com>
2020-03-02 22:37:50 +0100

    t

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function grid() {
   var container = document.querySelector('.container3');
-  cpt = 0;
+  var cpt = 0;
+  for (var j = 0; j < 4; j++) {
+    cpt++;
+    let div = document.createElement('div');
+    var classe = div.setAttribute("class", "grid-container grid-container--fill-" + cpt);
+    container.appendChild(div);
+  }
+
+}
+function grid2() {
+  var container = document.querySelector('.container2');
+  var cpt = 4;
   for (var j = 0; j < 4; j++) {
     cpt++;
     let div = document.createElement('div');
@@ -60,6 +71,58 @@ function addSeats4() {
     cpt++;
     var div = document.createElement('div');
     var Mine = div.setAttribute("class", "grid-element gridos" + cpt);
+    myWrapper.appendChild(div);
+
+  }
+
+}
+function addSeats5() {
+
+  var myWrapper = document.querySelector(".grid-container.grid-container--fill-5");
+  var cpt = 0;
+  for (var i = 0; i < 162; i++) {
+    cpt++;
+    var div = document.createElement('div');
+    var Mine = div.setAttribute("class", "grid-element gridos-" + cpt);
+    myWrapper.appendChild(div);
+
+  }
+
+}
+function addSeats6() {
+
+  var myWrapper = document.querySelector(".grid-container.grid-container--fill-6");
+  var cpt = 0;
+  for (var i = 0; i < 108; i++) {
+    cpt++;
+    var div = document.createElement('div');
+    var Mine = div.setAttribute("class", "grid-element gridos--" + cpt);
+    myWrapper.appendChild(div);
+
+  }
+
+}
+function addSeats7() {
+
+  var myWrapper = document.querySelector(".grid-container.grid-container--fill-7");
+  var cpt = 0;
+  for (var i = 0; i < 108; i++) {
+    cpt++;
+    var div = document.createElement('div');
+    var Mine = div.setAttribute("class", "grid-element gridose" + cpt);
+    myWrapper.appendChild(div);
+
+  }
+
+}
+function addSeats8() {
+
+  var myWrapper = document.querySelector(".grid-container.grid-container--fill-8");
+  var cpt = 0;
+  for (var i = 0; i < 153; i++) {
+    cpt++;
+    var div = document.createElement('div');
+    var Mine = div.setAttribute("class", "grid-element gridoses" + cpt);
     myWrapper.appendChild(div);
 
   }
